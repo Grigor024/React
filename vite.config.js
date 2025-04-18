4e381df9877f84a016580db91963359ddaecb2b7
Grigor <grigor024@gmail.com>
2025-04-18 12:38:00 +0400

    подключил новый репозиторий

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,  // Устанавливаем порт на 3000
+    port: 3000
   },
+  base: '/React/',
+
 })
