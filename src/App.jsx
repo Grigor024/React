7160185db2b1bcfc610a8227ddfe8707eb4d15b1
Grigor <grigor024@gmail.com>
2025-04-18 11:18:24 +0400

    Добавил проект

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExampleComponent from './components/ExampleComponent'
+
+function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Hello from React Starter + Tailwind!</h1>
+      <ExampleComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ExampleComponent() {
+  return <p className="text-lg">This is a reusable component.</p>
+}
+
+export default ExampleComponent
